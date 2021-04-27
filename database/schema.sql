517001e4be89e99c52e9ce05d65f3810f330f5a2
Night Chough <chicken.snuggles@gmail.com>
2021-04-27 12:08:31 -0500

    added foreign keys to schema

--- database/schema.sql
+++ database/schema.sql
@@ -5,21 +5,36 @@ CREATE DATABASE workforce;
 USE workforce;
 
 CREATE TABLE department (
-    id INT auto_increment PRIMARY KEY,
+    id INT NOT NULL auto_increment PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT auto_increment PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    INDEX depart_ind (department_id),
+    CONSTRAINT fkdepart
+    FOREIGN KEY (department_id)
+        REFERENCES department(id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-    id INT auto_increment PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-)
+    id INT NOT NULL auto_increment PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    INDEX role_ind (role_id),
+    INDEX manag_ind (manager_id),
+    CONSTRAINT fkrole
+    FOREIGN KEY (role_id)
+      REFERENCES role(id)
+      ON DELETE CASCADE,
+    CONSTRAINT fkmanager
+    FOREIGN KEY (manager_id)
+      REFERENCES employee(id)
+      ON DELETE SET NULL
+);
