ff4eac4b7e60eac84ca582f219d7332af460d44a
Night Chough <chicken.snuggles@gmail.com>
2021-04-27 16:45:42 -0500

    wrote prompts and queries for adding to workforce db

--- index.js
+++ index.js
@@ -58,15 +58,87 @@ function init() {
 }
 
 function addDepartment() {
-
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'newDepartment',
+                message: 'What is the name of the new department?'
+        }
+        ])
+        .then((answer) => {
+        
+            connection.query(''/*middleman insert into departments set ?*/, {
+                name: answer.newDepartment
+            })
+            console.log('New department added')
+        })
 };
 
 function addRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of the new role?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'What is the salary of the new role?'
+            },
+            {
+                type: 'input',
+                name: 'departmentId',
+                message: 'What is the department id of the new role?'
+            }
+        ])
+        .then((answer) => {
 
+            connection.query(''/*middleman insert into departments set ?*/, {
+                title: answer.newDepartment,
+                salary: answer.salary,
+                department_id: answer.departmentId
+            })
+            console.log('New role added')
+        })
 };
 
 function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'What is the first name of the new employee?'
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'What is the last name of the new employee?'
+            },
+            {
+                type: 'input',
+                name: 'roleId',
+                message: 'What is the role id of the new employee?'
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: 'What is the managers id of the new employee?'
+            }
+        ])
+        .then((answer) => {
 
+            connection.query(''/*middleman insert into departments set ?*/, {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.roleId,
+                manager_id: answer.managerId
+            })
+            console.log('New employee added')
+        })
 };
 
 function viewDepartments() {
