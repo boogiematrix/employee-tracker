0bc4509e39fcd0ab267c8b6839fd97cc0c91e9cc
Night Chough <chicken.snuggles@gmail.com>
2021-04-27 15:31:00 -0500

    filled in seeds.sql

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,19 +1,27 @@
 USE workforce
 
 INSERT INTO department (name)
-VALUES ()
-()
-()
-();
+VALUES ('R&D'),
+('Marketing'),
+('Accounting'),
+('Human Resources');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ()
-()
-()
-();
+VALUES ('Head Researcher', 200000, 1),
+('Lab technician', 120000, 1),
+('Head of Marketing', 200000, 2),
+('Ad Representative', 100000, 2),
+('Lead Accountant', 140000, 3),
+('Assistant Accountant', 90000, 3),
+('Head of HR', 150000, 4),
+('Recruiter', 110000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ()
-()
-()
-();
+VALUES ('Jim', 'Jam', 1, null),
+('Tim', 'Tam', 2, 1),
+('Jenny', 'Blake', 3, null),
+('Terry', 'Berry', 4, 3),
+('Angela', 'Martin', 5, null),
+('Kevin', 'Malone', 6, 5),
+('Sonja', 'Blade', 7, null),
+('Armand', 'Stein', 8, 7);
