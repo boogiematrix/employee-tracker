383bb94f26f6f801c19c7f9561fd37f61b201613
Night Chough <chicken.snuggles@gmail.com>
2021-04-27 16:58:17 -0500

    added query contents to class.js

--- database/class.js
+++ database/class.js
@@ -1,1 +1,35 @@
-//to contain all query contents
+class Middleman {
+    constructor() {
+        
+    };
+
+    createDepartment() {
+        return 'INSERT INTO department SET ?'
+    };
+    
+    createRole() {
+        return 'INSERT INTO role SET ?'
+    };
+    
+    createEmployee() {
+        return 'INSERT INTO employee SET ?'
+    };
+
+    readDepartments() {
+        return 'SELECT * FROM department'
+    };
+
+    readRoles() {
+        return 'SELECT * FROM role'
+    };
+
+    readEmployees() {
+        return 'SELECT * FROM employee'
+    };
+
+    updateEmployeeRole() {
+        return 'UPDATE employee SET ?'
+    }
+}
+
+module.exports = new Middleman
