0f0fc0bf70332535fec3a61275167d125e16da28
Night Chough <chicken.snuggles@gmail.com>
2021-04-27 15:35:06 -0500

    added connection

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 const middleMan = require('./database/class')
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+
+    port: 3306,
+
+    user: 'root',
+
+    password: 'mingmang0',
+    database: 'workforce',
+});
+
+
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    init()
+});
