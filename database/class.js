912500ba88905519efc37cc6ad69795b7f54b509
Night Chough <chicken.snuggles@gmail.com>
2021-04-29 12:18:40 -0500

    added query strings for new functions

--- database/class.js
+++ database/class.js
@@ -27,13 +27,45 @@ class Middleman {
         return 'SELECT * FROM employee'
     };
 
+    readEmployeeByManager() {
+        return 'SELECT * FROM employee WHERE manager_id = ?'
+    };
+
+    readDeptSalaryBudget() {
+        return 'SELECT SUM(role.salary) FROM department\
+        JOIN role\
+        ON department.id = role.department_id\
+        join employee\
+        ON role.id = employee.role_id\
+        WHERE department.id = ?;'
+    };
+
     readEmployeesAndRoles() {
-        return 'SELECT employee.id AS "employeeId", employee.first_name AS first_name, employee.last_name AS last_name, role.id AS roleId, role.title AS title FROM employee INNER JOIN role ON role.id = employee.role_id'
-    }
+        return 'SELECT employee.id AS "employeeId", employee.first_name AS first_name, employee.last_name AS last_name, role.id AS roleId, role.title AS title\
+        FROM employee\
+         INNER JOIN role\
+        ON role.id = employee.role_id'
+    };
 
     updateEmployeeRole() {
         return 'UPDATE employee SET role_id = ? WHERE id = ?'
-    }
+    };
+
+    updateManagers() {
+        return 'UPDATE emplyee SET manager_id = ? WHERE id = ?'
+    };
+
+    destroyDepartment() {
+        return 'DELETE FROM department WHERE id = ?'
+    };
+
+    destroyRole() {
+        return 'DELETE FROM role WHERE id = ?'
+    };
+
+    destroyEmployee() {
+        return 'DELETE FROM employee WHERE id = ?'
+    };
 }
 
 module.exports = new Middleman
